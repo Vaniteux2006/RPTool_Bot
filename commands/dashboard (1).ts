4cb1f439ddba14b8e167f6e34be2ed99407594d6
Ivan <67961643+Mrvan999@users.noreply.github.com>
2026-03-29 11:13:17 -0300

    Update Dashboard Command

---
+++ commands/dashboard (1).ts
@@ -0,0 +1,37 @@
+export interface AuditStats {
+    bans: number
+    warns: number
+    roles: number
+}
+
+export interface EventStats {
+    messages: number
+    calls: number
+    joins: number
+}
+
+export interface TopUser {
+    name: string
+    messages: number
+    call: number
+    avatar?: string
+}
+
+export interface ActivityStats {
+    hours: string[]
+    messages: number[]
+    calls: number[]
+}
+
+export interface InactiveChannel {
+    name: string
+    time: string
+}
+
+export interface DashboardStats {
+    audit: AuditStats
+    events: EventStats
+    topUsers: TopUser[]
+    activity: ActivityStats
+    inactiveChannels: InactiveChannel[]
+}
